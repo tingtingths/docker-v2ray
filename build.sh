bbad2718c54670b96ef286833e788129256e6489
Tony Ting <k999hoshing@gmail.com>
2021-08-19 12:33:34 +0800

    build: add build script.

--- build.sh
+++ build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env sh
-docker build -t tingtingths/v2ray:$1 --build-arg VERSION=$1 .
+
+if [ -z "$1" ]; then
+	echo 'Missing version argument, e.g. build.sh v4.33.0'
+    exit 1
+fi
+
+echo "Building version ${1}..."
+sleep 3
+docker build --no-cache -t registry.itdog.me/v2ray:$1 --build-arg VERSION=$1 . \
+	&& docker tag registry.itdog.me/v2ray:$1 registry.itdog.me/v2ray:latest \
+	&& docker push registry.itdog.me/v2ray:$1 \
+	&& docker push registry.itdog.me/v2ray:latest
